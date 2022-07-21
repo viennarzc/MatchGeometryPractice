c5f34114db8ca457303a1d2e98bf6f10efc65f7a
Viennarz Curtiz <viennarzc@gmail.com>
2022-07-21 08:09:40 +0800

    Initial Commit

---
+++ MatchGeometryPractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MatchGeometryPractice
+//
+//  Created by Viennarz Curtiz on 7/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MatchGeometryPractice/MatchGeometryPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchGeometryPracticeApp.swift
+//  MatchGeometryPractice
+//
+//  Created by Viennarz Curtiz on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct MatchGeometryPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
